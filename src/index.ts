1f13428f644dcd88803105dc5eaff8a7f65fe099
mTadeja <mateusz.tadeja@gmail.com>
2018-11-18 15:39:01 +0100

    refactor(export): export class instead of function

--- src/index.ts
+++ src/index.ts
@@ -1,11 +1,28 @@
-import {CountryValidation} from './_classes/CountryValidation';
+import {companyOrganizationForms} from './companyOrganizationForms/companyOrganizationForms'
 import {countries} from './countries';
+import {documents} from './documents/documents'
+import {IValidator} from './_interfaces/validator.interface';
+import {zipCodeValidators} from './zipCodeValidators/zipCodeValidators';
+import {personalIdentification} from './personalIdentification/personalIdentification';
 
-export function getCountryValidation(countryIsoCode: string) {
-    countryIsoCode = countryIsoCode.toUpperCase();
-    if (countries[countryIsoCode]) {
-        return new CountryValidation(countryIsoCode);
-    }
-    console.warn('Country currently unhandled. Please send email to mateusz.tadeja@gmail.com or fork and create' +
+export class CountryValidation {
+  public zipCode: IValidator;
+  public companyOrganizationForms: Array<string>;
+  public documents: Array<IValidator>;
+  public isoCountryCode: string;
+  public countryName: string;
+  public personalIdentification: IValidator;
+
+  constructor(countryIsoCode: string) {
+    if (!countries[countryIsoCode]) {
+      throw new Error('Country currently unhandled. Please send email to mateusz.tadeja@gmail.com or fork and create' +
         'a pull request at https://github.com/RudiSzubert/worldwidevalidations if you need it');
+    }
+    this.isoCountryCode = countryIsoCode;
+    this.countryName = countries[countryIsoCode];
+    this.zipCode = zipCodeValidators[countryIsoCode];
+    this.documents = documents[countryIsoCode];
+    this.companyOrganizationForms = companyOrganizationForms[countryIsoCode];
+    this.personalIdentification = personalIdentification[countryIsoCode];
+  }
 }
