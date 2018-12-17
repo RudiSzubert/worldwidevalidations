7cbac5b4bad6218aecdf8634b511c674d0ae69fa
mTadeja <mateusz.tadeja@gmail.com>
2018-12-17 22:07:07 +0100

    chore(): fix zipcode regex

--- src/zipCodeValidators/pl/validator.ts
+++ src/zipCodeValidators/pl/validator.ts
@@ -2,7 +2,7 @@ import {IValidator} from '../../_interfaces/validator.interface';
 
 export const validator: IValidator = {
     validate: (zipCode: string): boolean => {
-        return !!zipCode.match('[0-9]{2}\\-[0-9]{3}$');
+        return !!zipCode.match('^[0-9]{2}\\-[0-9]{3}$');
     },
     name: 'Poland zip code validator',
     format: '00-000'
