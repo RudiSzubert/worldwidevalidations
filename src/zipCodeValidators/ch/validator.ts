f15b6ad07fd5a4c32fe03b55d359899c22057ad3
Mateusz Tadeja <mateusz.tadeja@neoteric.eu>
2020-03-05 14:56:02 +0100

    fix(CH zipcode validator):

--- src/zipCodeValidators/ch/validator.ts
+++ src/zipCodeValidators/ch/validator.ts
@@ -2,7 +2,7 @@ import {IValidator} from '../../_interfaces/validator.interface';
 
 export const validator: IValidator = {
     validate: (zipCode: string): boolean => {
-        return !!zipCode.match('/^[1-9]\\d{3}$/');
+        return !!zipCode.match('^[1-9][0-9][0-9][0-9]$');
     },
     name: 'Swiss zip code validator',
     format: '0000'
