9933b4c5e23e6b71dc94a259a5f30fc2939258fe
Mateusz Tadeja <mateusz.tadeja@neoteric.eu>
2020-03-05 15:17:01 +0100

    chore(CH): change tin format, and AVS validators

--- src/documents/ch/tin.ts
+++ src/documents/ch/tin.ts
@@ -3,7 +3,7 @@ import { IValidator } from '../../_interfaces/validator.interface';
 export const tin: IValidator = {
     label: 'TIN',
     name: 'Tax Identification number',
-    format: '000000000',
+    format: '000-00-0000',
     validate: (tinNumber: string): boolean => {
         return !!tinNumber.match('^(9\\d{2})([ \\-]?)([7]\\d|8[0-8])([ \\-]?)(\\d{4})$');
     }
--- src/personalIdentification/ch/validator.ts
+++ src/personalIdentification/ch/validator.ts
@@ -2,9 +2,10 @@ import { IValidator } from '../../_interfaces/validator.interface';
 
 export const validator: IValidator = {
       validate: (avs: string): boolean => {
-        return !!avs.match('^[0-9]{2}\\-[0-9]{3}$');
+        return !!(avs.match('756\\.?[0-9]{4}\\.?[0-9]{4}\\.?[0-9]{2}') || // new code (since 2008)
+            avs.match('[0-9]{3}\\.?[0-9]{2}\\.?[0-9]{3}\\.?[0-9]{3}'));// old one
       },
-  name: 'Swiss social security number validator',
-  format: '000.000.000.000',
+  name: 'AVS - Swiss social security number validator',
+  format: '000.000.000.000 or 756.0000.0000.00',
   label: 'AVS'
 };
