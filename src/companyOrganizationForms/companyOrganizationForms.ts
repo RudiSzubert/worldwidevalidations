e13d204c2979c09b9aac7a68617bc26d11d6fbe3
Mateusz Tadeja <mateusz.tadeja@neoteric.eu>
2020-03-22 15:02:35 +0100

    chore(de): add german company organisation forms

--- src/companyOrganizationForms/companyOrganizationForms.ts
+++ src/companyOrganizationForms/companyOrganizationForms.ts
@@ -1,7 +1,9 @@
 import {companyOrganizationForms as plList} from './pl/list';
 import {companyOrganizationForms as chList} from './ch/list';
+import {companyOrganizationForms as deList} from './de/list';
 
 export const companyOrganizationForms: {[key: string]: Array<string>} = {
     PL: plList,
     CH: chList,
+    DE: deList,
 };
---
+++ src/companyOrganizationForms/de/list.ts
@@ -0,0 +1,7 @@
+export const companyOrganizationForms: Array<string> = [
+    'Limited Liability Company (Gesellschaft mit beschränkter Haftung) - GmbH',
+    'Joint Stock Company (Aktiengesellschaft) - AG',
+    'Limited Partnership (Kommanditgesellschaft) - KG',
+    'General Partnership (Offene Handelsgesellschaft) - oHG',
+    'Civil law Partnership (Gesellschaft bürgerlichen Rechts) - GbR'
+];
